e277d1e9e397ffdd9afb522fe46d7e2ebd05558a
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-10-12 12:47:56 -0300

    :sparkles: feat(routes - index.ts): add new route for documentation, the swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { HealthCheck } from "./healthCheckRouter";
+import swaggerUi from "swagger-ui-express";
+
 const routes = Router();
 
 routes.use("/healthCheck", new HealthCheck().check);
+routes.use("/api-docs", swaggerUi.serve,swaggerUi.setup());
 
 export default routes;
