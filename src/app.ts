5d88f6d492bcf82590d282bc6b27eaf7c9ff9880
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-10-12 10:54:16 -0300

    :sparkles: feat(app.ts): add helmet as a middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import morgan from "morgan";
 import compression from "compression";
 import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 // app.disable("x-powered-by");
 app.use(express.json());
