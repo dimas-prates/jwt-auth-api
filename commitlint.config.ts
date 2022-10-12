429320bca2f260d2bf6a093be85d31a1ad63efe4
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-10-12 09:43:45 -0300

    :pencil2: style: format document

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,9 +2,9 @@ import type { UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
   /*
-	 * Resolve and load @commitlint/config-conventional from node_modules.
-	 * Referenced packages must be installed
-	 */
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
   extends: ["gitmoji"],
   /*
   * Any rules defined here will override rules from @commitlint/config-conventional
