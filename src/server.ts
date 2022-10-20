e67c81d0e4f5b6eb6172ba020013482ec4b21ab9
Dimas <31516927+dimas-prates@users.noreply.github.com>
2022-10-20 14:37:16 -0300

    :sparkles: feat(server.ts): create shutdown function, add database connection and destruction

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,47 @@
 import "dotenv/config";
 import app from "./app";
+import Database from "./database/data-source";
 
 const appPort = process.env.PORT || 3000;
 const processId = process.pid;
-
 const server = app.listen(appPort, () => {
   console.log(`Server's (PID ${processId}) running on PORT:${appPort}`);
 });
+
+Database.initialize().then(async (connection) => {
+  const { type, database } = connection.options;
+  console.log(`Database ${database} (${type}): ${connection.isInitialized ? "connected" : "disconnect"}\n`);
+}).catch(error => console.log(error));
 
 function gracefulShutdown(event: string) {
   return (signal: NodeJS.Signals) => {
     server.close(async () => {
       console.log(`App (PID:${processId}) received: ${signal}`);
-      // console.log(`Database status: ${Database.isInitialized ? "connected" : "disconnect"}\n`);
-      // await Database.destroy();
+      await Database.destroy();
+      console.log(`Database status: ${Database.isInitialized ? "connected" : "disconnect"}\n`);
       process.exit(0);
     });
   };
+}
+
+function shutdown() {
+  server.close(async () => {
+    await Database.destroy();
+    console.log(`Database status: ${Database.isInitialized ? "connected" : "disconnect"}\n`);
+    process.exit(1);
+  });
+  setTimeout(() => { process.exit(1); }, 1000).unref();
 }
 
 process.on("SIGINT", gracefulShutdown("SIGINT"));
 process.on("SIGTERM", gracefulShutdown("SIGTERM"));
 
 process.on("uncaughtException", (error, origin) => {
-  server.close(async () => {
-    console.log(`App (PID: ${processId}) received: ${origin}\n${error}`);
-    // console.log(`Database status: ${Database.isInitialized ? "connected" : "disconnect"}\n`);
-    // await Database.destroy();
-    process.exit(1);
-  });
-  setTimeout(() => { process.exit(1); }, 1000).unref();
+  console.log(`App (PID: ${processId}) received: ${origin}\n${error}`);
+  shutdown();
 });
 
 process.on("unhandledRejection", (error) => {
-  server.close(async () => {
-    console.log(`App (PID: ${processId}) received: ${error}`);
-    // console.log(`Database status: ${Database.isInitialized ? "connected" : "disconnect"}\n`);
-    // await Database.destroy();
-    process.exit(1);
-  });
-  setTimeout(() => { process.exit(1); }, 1000).unref();
+  console.log(`App (PID: ${processId}) received: ${error}`);
+  shutdown();
 });
